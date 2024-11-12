8718c6d4d9025774160d600471d67cc7a90d5d3a
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-11-12 15:57:07 +0100

    add footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This app was coded by <a href="/">Georgina Mudditt</a>, is open-sourced
+        on <a href="/">GitHub</a>, and hosted on <a href="/">Netlify</a>.
+      </p>
+    </div>
+  );
+}
