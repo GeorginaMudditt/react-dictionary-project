f651bd673482e455c08a7a5a32f51b2629fe60d7
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-11-12 16:01:52 +0100

    add links to footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,31 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>
-        This app was coded by <a href="/">Georgina Mudditt</a>, is open-sourced
-        on <a href="/">GitHub</a>, and hosted on <a href="/">Netlify</a>.
+        This app was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/georgina-mudditt/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Georgina Mudditt
+        </a>
+        , is open-sourced on{" "}
+        <a
+          href="https://github.com/GeorginaMudditt/react-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted on{" "}
+        <a
+          href="https://gm-dictionary.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
       </p>
     </div>
   );
